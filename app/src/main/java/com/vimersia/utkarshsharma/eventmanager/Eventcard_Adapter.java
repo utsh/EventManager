fe8cf75f13d16a37dee408f398c71a88177bbd82
utsh <longliveutkarsh@gmail.com>
2017-03-06 23:33:36 +0530

    working : online db

---
+++ app/src/main/java/com/vimersia/utkarshsharma/eventmanager/Eventcard_Adapter.java
@@ -0,0 +1,76 @@
+package com.vimersia.utkarshsharma.eventmanager;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by UTKARSH SHARMA on 3/6/2017.
+ */
+
+public class Eventcard_Adapter extends RecyclerView.Adapter<Eventcard_Adapter.ViewHolder> {
+    ArrayList<HashMap<String, String>> personList;
+
+
+// Provide a reference to the views for each data item
+// Complex data items may need more than one view per item, and
+// you provide access to all the views for a data item in a view holder
+public static class ViewHolder extends RecyclerView.ViewHolder {
+    // each data item is just a string in this case
+    public TextView EventNameTextView,EventDateTextView,EventTimeTextView,EventVenueTextView;
+    public ImageView EventImageView;
+    public ViewHolder(View view) {
+        super(view);
+        EventNameTextView = (TextView) view.findViewById(R.id.event_name_inflate);
+        EventDateTextView = (TextView) view.findViewById(R.id.event_date_inflate);
+        EventTimeTextView = (TextView) view.findViewById(R.id.event_time_inflate);
+        EventVenueTextView = (TextView) view.findViewById(R.id.event_address);
+        EventImageView = (ImageView) view.findViewById(R.id.image_card);
+    }
+
+}
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public Eventcard_Adapter(ArrayList<HashMap<String, String>> myDataset) {
+        personList = myDataset;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public Eventcard_Adapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        // create a new view
+        View v =  LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_main, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        String name =personList.get(position).get("TAG_Name");
+        holder.EventNameTextView.setText(personList.get(position).get("TAG_Name"));
+        holder.EventDateTextView.setText(personList.get(position).get("TAG_Date"));
+        holder.EventTimeTextView.setText(personList.get(position).get("TAG_Time"));
+        holder.EventVenueTextView.setText(personList.get(position).get("TAG_Address"));
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return personList.size();
+    }
+}
---
+++ app/src/main/java/com/vimersia/utkarshsharma/eventmanager/MainActivity.java
@@ -0,0 +1,196 @@
+package com.vimersia.utkarshsharma.eventmanager;
+
+import android.app.usage.UsageEvents;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import org.apache.http.*;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    String myJSON;
+
+    private static final String TAG_RESULTS="result";
+    private static final String TAG_Name = "Event_name";
+    private static final String TAG_Address = "Event_Address";
+    private static final String TAG_Date = "Event_Date";
+    private static final String TAG_Time = "Event_Time";
+    JSONArray peoples = null;
+    HashMap<String,String> persons ;
+    ArrayList<HashMap<String, String>> personList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView)  findViewById(R.id.my_recycler_view);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        personList = new ArrayList<HashMap<String,String>>();
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        String [] myDataset = new String[4];
+        myDataset[0]="Google";
+        myDataset[1]="1/12/13";
+        myDataset[2]="10:30";
+        myDataset[3]="jbchkevbwev hhwirgubwev qeifbeifeufevfu";
+
+
+        getData();
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+    protected void showList(){
+        try {
+            JSONObject jsonObj = new JSONObject(myJSON);
+            peoples = jsonObj.getJSONArray(TAG_RESULTS);
+
+            for(int i=0;i<peoples.length();i++){
+                JSONObject c = peoples.getJSONObject(i);
+                String name = c.getString(TAG_Name);
+                String date = c.getString(TAG_Date);
+                String time = c.getString(TAG_Time);
+                String address = c.getString(TAG_Address);
+               /* Vr_Live_Database ldb = new Vr_Live_Database(getBaseContext());
+                ldb.createRecords(id,name);
+                Cursor mcur=ldb.selectRecords();*/
+                persons= new HashMap<String,String>();
+                persons.put("TAG_Name",name);
+                persons.put("TAG_Date",date);
+                persons.put("TAG_Time",time);
+                persons.put("TAG_Address",address);
+                personList.add(persons);
+
+
+
+            }
+
+            mAdapter = new Eventcard_Adapter(personList);
+
+            mRecyclerView.setAdapter(mAdapter);
+            mRecyclerView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    startEventClass();
+                }
+            });
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void getData(){
+        class GetDataJSON extends AsyncTask<String, Void, String> {
+
+            @Override
+            protected String doInBackground(String... params) {
+                DefaultHttpClient httpclient = new DefaultHttpClient(new BasicHttpParams());
+                HttpPost httppost = new HttpPost("http://www.vimersia.com/eventdb.php");
+                // Depends on your web service
+                httppost.setHeader("Content-type", "application/json");
+
+                InputStream inputStream = null;
+                String result = null;
+                try {
+                    HttpResponse response = httpclient.execute(httppost);
+                    HttpEntity entity = response.getEntity();
+
+                    inputStream = entity.getContent();
+                    // json is UTF-8 by default
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+                    StringBuilder sb = new StringBuilder();
+
+                    String line = null;
+                    while ((line = reader.readLine()) != null)
+                    {
+                        sb.append(line );
+                    }
+                    result = sb.toString();
+                } catch (Exception e) {
+
+                    // Oops
+
+                }
+                finally {
+                    try{if(inputStream != null)inputStream.close();}catch(Exception squish){}
+                }
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(String result){
+                myJSON=result;
+                showList();
+            }
+        }
+        GetDataJSON g = new GetDataJSON();
+        g.execute();
+    }
+public void startEventClass() {
+    Intent Eventclass = new Intent(this, Event_Details.class);
+    startActivity(Eventclass);
+}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
